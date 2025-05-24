975e0c9a593174a4a2e8f6814af32785eb3e109a
richard <richardsatilite.dev@gmail.com>
2025-05-24 12:18:50 -0300

    Defined some functions in header file

---
+++ RBTree.h
@@ -0,0 +1,32 @@
+#ifndef RBTREE_H
+#define RBTREE_H
+
+
+// Definindo dado do tipo Color //
+typedef enum Color{
+	RED,
+	BLACK
+}Color;
+
+
+// Estrutura de um no da arvore rubro-negra //
+typedef Node{
+	int val;
+	Color color;
+	struct Node* left;
+	struct Node* right;
+	struct Node* parent;	
+}Node;
+
+
+// Funcao que efetua uma rotacao a direita //
+// Recebe um ponteiro do tipo Node que causa inviolacao //
+void rightRotate(Node *node);
+
+
+// Funcao que efetua uma rotacao a esquerda //
+// Recebe um ponteiro do tipo Node que causa inviolacao //
+void leftRotate(Node *node);
+
+
+#endif
